59f3eab2dc88e291aefd866ec6e4bee686cd3ed0
Luna2225 <lunnasantanna981@gmail.com>
2022-06-28 19:49:15 -0300

    modificacao da pagina editais

--- src/pages/Edital.js
+++ src/pages/Edital.js
@@ -1,29 +1,29 @@
 import * as React from 'react';
-import { Text, View, TouchableHighlight, StyleSheet } from 'react-native'
+import { Text, View, TouchableOpacity, StyleSheet } from 'react-native'
 
 export default props => {
     return (
         <View style={style.campus}>
-            <TouchableHighlight style={style.btn1} onPress={() => { props.navigation.navigate("Edital") }}>
+            <TouchableOpacity style={style.btn} onPress={() => { props.navigation.navigate("Edital") }}>
                 <View>
                     <Text>Edital Nº XX/XXXX</Text>
                 </View>
-            </TouchableHighlight>
-            <TouchableHighlight style={style.btn2} onPress={() => { props.navigation.navigate("Edital") }}>
+            </TouchableOpacity>
+            <TouchableOpacity style={style.btn} onPress={() => { props.navigation.navigate("Edital") }}>
                 <View>
                     <Text>Edital Nº XX/XXXX</Text>
                 </View>
-            </TouchableHighlight>
-            <TouchableHighlight style={style.btn3} onPress={() => { props.navigation.navigate("Edital") }}>
+            </TouchableOpacity>
+            <TouchableOpacity style={style.btn} onPress={() => { props.navigation.navigate("Edital") }}>
                 <View>
                     <Text>Edital Nº XX/XXXX</Text>
                 </View>
-            </TouchableHighlight>
-            <TouchableHighlight style={style.btn4} onPress={() => { props.navigation.navigate("Edital") }}>
+            </TouchableOpacity>
+            <TouchableOpacity style={style.btn} onPress={() => { props.navigation.navigate("Edital") }}>
                 <View>
                     <Text>Edital Nº XX/XXXX</Text>
                 </View>
-            </TouchableHighlight>
+            </TouchableOpacity>
 
         </View>
     )
@@ -33,29 +33,11 @@ const style = StyleSheet.create({
     campus: {
         flex: 1,
     },
-    btn1: {
+    btn: {
         textAlign: 'center',
         padding: 20,
         borderRadius: 30,
-        backgroundColor: 'DarkCyan'
-    },
-    btn2: {
-        textAlign: 'center',
-        padding: 20,
-        borderRadius: 30,
-        backgroundColor: 'DarkCyan'
-    },
-    btn3: {
-        textAlign: 'center',
-        padding: 20,
-        borderRadius: 30,
-        backgroundColor: 'DarkCyan'
-    },
-    btn4: {
-        textAlign: 'center',
-        padding: 20,
-        borderRadius: 30,
-        backgroundColor: 'DarkCyan'
+        backgroundColor: '#1A8FDD'
     }
 })
 
