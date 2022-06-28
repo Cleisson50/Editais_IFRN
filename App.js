588af8e7ad11342ae4d43d882c1a8686e2238a9d
Luna2225 <lunnasantanna981@gmail.com>
2022-06-27 21:30:01 -0300

    Alterando a pagina de editais, stack e app

--- App.js
+++ App.js
@@ -1,20 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
+import Stack from './navigation/Stack';
+import { NavigationContainer } from '@react-navigation/native';
 
-export default function App() {
+export default () => {
   return (
-    <View style={styles.container}>
-      <Text>Editais IFRN</Text>
-      <StatusBar style="auto" />
+    <View>
+      <NavigationContainer>
+        <Stack />
+      </NavigationContainer>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- navigation/Stack.js
+++ navigation/Stack.js
@@ -1,13 +1,17 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Index from '../src/pages/Index';
+import Campus from '../src/pages/Campus';
+import Ano from '../src/pages/Ano';
+import Edital from '../src/pages/Edital';
 
 
 const Stack = createNativeStackNavigator();
 
 export default (props) => (
     <Stack.Navigator initialRouteName="Index">
-      <Stack.Screen name="Contato" component={Contato} />
-      <Stack.Screen name="Sobre" component={Sobre} />
+      <Stack.Screen name="Campus" component={Campus} />
+      <Stack.Screen name="Ano" component={Ano} />
       <Stack.Screen name="Index" component={Index} />
+      <Stack.Screen name="Edital" component={Edital} />
     </Stack.Navigator>
   );
