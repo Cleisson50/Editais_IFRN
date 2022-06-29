1a28c421ff1e90fbc02f520c011641ae5064d22f
Daiane3006 <daiane.correia.oliveira77@gmail.com>
2022-06-28 22:07:50 -0300

    Mudando os butões de TouchableHighlight para TouchableOpacity na página Campus

--- src/pages/Campus.js
+++ src/pages/Campus.js
@@ -1,29 +1,29 @@
 import * as React from 'react';
-import { Text, View, TouchableHighlight, StyleSheet } from 'react-native'
+import { Text, View, TouchableOpacity, StyleSheet } from 'react-native'
 
 export default props => {
     return (
         <View style={style.campus}>
-            <TouchableHighlight style={style.btn1} onPress={() => { props.navigation.navigate("Ano") }}>
+            <TouchableOpacity style={style.btn1} onPress={() => { props.navigation.navigate("Ano") }}>
                 <View>
                     <Text>Campus Apodi</Text>
                 </View>
-            </TouchableHighlight>
-            <TouchableHighlight style={style.btn2} onPress={() => { props.navigation.navigate("Ano") }}>
+            </TouchableOpacity>
+            <TouchableOpacity style={style.btn2} onPress={() => { props.navigation.navigate("Ano") }}>
                 <View>
                     <Text>Campus Caicó</Text>
                 </View>
-            </TouchableHighlight>
-            <TouchableHighlight style={style.btn3} onPress={() => { props.navigation.navigate("Ano") }}>
+            </TouchableOpacity>
+            <TouchableOpacity style={style.btn3} onPress={() => { props.navigation.navigate("Ano") }}>
                 <View>
                     <Text>Campus Canguaretama</Text>
                 </View>
-            </TouchableHighlight>
-            <TouchableHighlight style={style.btn4} onPress={() => { props.navigation.navigate("Ano") }}>
+            </TouchableOpacity>
+            <TouchableOpacity style={style.btn4} onPress={() => { props.navigation.navigate("Ano") }}>
                 <View>
                     <Text>Campus Lajes</Text>
                 </View>
-            </TouchableHighlight>
+            </TouchableOpacity>
 
         </View>
     )
