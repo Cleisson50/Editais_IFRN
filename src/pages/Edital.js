f73f094bb3f408d7da7b146c4f58745a4727969d
Luna2225 <lunnasantanna981@gmail.com>
2022-06-28 21:26:09 -0300

    estilizando a pagina edital

--- src/pages/Edital.js
+++ src/pages/Edital.js
@@ -8,22 +8,22 @@ export default props => {
                 <Text>Editais IFRN /
                     Campus / Ano / Edital</Text>
             </View>
-            <TouchableOpacity style={style.btn} onPress={() => { props.navigation.navigate("Edital") }}>
+            <TouchableOpacity style={style.btn1} onPress={() => { props.navigation.navigate("Edital") }}>
                 <View>
                     <Text>Edital Nº XX/XXXX</Text>
                 </View>
             </TouchableOpacity>
-            <TouchableOpacity style={style.btn} onPress={() => { props.navigation.navigate("Edital") }}>
+            <TouchableOpacity style={style.btn2} onPress={() => { props.navigation.navigate("Edital") }}>
                 <View>
                     <Text>Edital Nº XX/XXXX</Text>
                 </View>
             </TouchableOpacity>
-            <TouchableOpacity style={style.btn} onPress={() => { props.navigation.navigate("Edital") }}>
+            <TouchableOpacity style={style.btn3} onPress={() => { props.navigation.navigate("Edital") }}>
                 <View>
                     <Text>Edital Nº XX/XXXX</Text>
                 </View>
             </TouchableOpacity>
-            <TouchableOpacity style={style.btn} onPress={() => { props.navigation.navigate("Edital") }}>
+            <TouchableOpacity style={style.btn4} onPress={() => { props.navigation.navigate("Edital") }}>
                 <View>
                     <Text>Edital Nº XX/XXXX</Text>
                 </View>
@@ -38,13 +38,36 @@ const style = StyleSheet.create({
         flex: 1,
     },
     header: {
+        marginBottom: 20,
         textAlign: 'center',
         padding: 30,
         backgroundColor: '#4AF56F'
     },
-    btn: {
+    btn1: {
+        marginBottom: 20,
         textAlign: 'center',
-        padding: 20,
+        padding: 15,
+        borderRadius: 30,
+        backgroundColor: '#1A8FDD'
+    },
+    btn2: {
+        marginBottom: 20,
+        textAlign: 'center',
+        padding: 15,
+        borderRadius: 30,
+        backgroundColor: '#1A8FDD'
+    },
+    btn3: {
+        marginBottom: 20,
+        textAlign: 'center',
+        padding: 15,
+        borderRadius: 30,
+        backgroundColor: '#1A8FDD'
+    },
+    btn4: {
+        marginBottom: 20,
+        textAlign: 'center',
+        padding: 15,
         borderRadius: 30,
         backgroundColor: '#1A8FDD'
     }
